e2441039e413f14e88a52b504064a76f05918718
eran broder <eran.b@walkme437.mynet>
2018-11-20 20:36:54 +0200

    Initial commit

---
+++ Example/MyFirstPod/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MyFirstPod
+//
+//  Created by Eran Broder on 11/20/2018.
+//  Copyright (c) 2018 Eran Broder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
